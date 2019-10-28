13b11040b53bba4f4352a49325eb73a63f6f070c
arley <34805850@qq.com>
2019-10-28 17:32:48 +0800

    call ↑

--- MyCall.js
+++ MyCall.js
@@ -3,16 +3,18 @@
  * 改变了 this 指向，让新的对象可以执行该函数，并能接受参数
  */
 
-Function.prototype.MyCall = function(context) {
+Function.prototype.MyCall = function(...args) {
+  const isWeb = () => (typeof window !== 'undefined')
+
   if (typeof this !== 'function') {
-    throw new TypeError('Error')
+    throw new TypeError('Call can only be called by a function') // 1. 什么情况下能触发这个错误 ?
   }
-  context = context || global // window
-  const args = [...arguments].slice(1)
-  context.fn = this // this为要执行的函数, 谁执行了这个函数, 函数中的this就指向谁
-  const result = context.fn(...args)
+  const context = args[0] || (isWeb() ? window : global) // 2. 如何让call支持浏览器且支持nodejs
+  context.fn = this
+  const result = context.fn(...args.slice(1))
   delete context.fn
   return result
 }
 
 console.log(Math.max.MyCall(null, 10, 9, 8, 100, 0, -1))
+
